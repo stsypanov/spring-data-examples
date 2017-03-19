96e365acdc3f853144bbb53d6d0d299306e197dc
stsypanov <STsypanov@luxoft.com>
2017-03-19 12:29:01 +0200

    add assertion

--- src/test/java/com/luxoft/logeek/PupilRepositoryTest.java
+++ src/test/java/com/luxoft/logeek/PupilRepositoryTest.java
@@ -3,7 +3,11 @@ package com.luxoft.logeek;
 import com.luxoft.logeek.entity.Pupil;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
 
 @SuppressWarnings("unused")
 public class PupilRepositoryTest extends TestBase {
@@ -14,10 +18,12 @@ public class PupilRepositoryTest extends TestBase {
     @Test
     public void findUnordered() throws Exception {
         Set<Pupil> pupilsOlderThan = pupilRepository.findPupilsOlderThan(3);
+        assertEquals(HashSet.class.getName(), pupilsOlderThan.getClass().getName());
     }
 
     @Test
     public void findOrdered() throws Exception {
         Set<Pupil> pupilsOlderThan = pupilRepository.findPupilsOlderThanOrderedByAge(3);
+        assertEquals(LinkedHashSet.class.getName(), pupilsOlderThan.getClass().getName());
     }
 }
