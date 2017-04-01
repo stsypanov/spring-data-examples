cc31146c4cc8ee9b935de8a0212f3524ec2cb855
stsypanov <sergei.tsypanov@yandex.ru>
2017-04-01 14:04:10 +0300

    add more methods

--- src/main/java/com/luxoft/logeek/repository/ChildRepository.java
+++ src/main/java/com/luxoft/logeek/repository/ChildRepository.java
@@ -7,17 +7,28 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-@SuppressWarnings({"JpaQlInspection", "SameParameterValue"})
+@SuppressWarnings({"SameParameterValue"})
 public interface ChildRepository extends JpaRepository<Child, Long>, ChildRepositoryCustom {
 
 	List<Child> findByParentName(String name);
 
 	@Query("select c from Child c " +
-			"join c.parent parent " +
+			" join c.parent parent " +
 			"where parent.name = :name")
 	List<Child> findByParentNameWithExplicitJoin(@Param("name") String name);
 
 	@Query("select c from Child c " +
 			"where c.parent.name = :name")
 	List<Child> findByParentNameWithoutExplicitJoin(@Param("name") String name);
+
+	List<Child> findByParentId(Long id);
+
+	@Query("select c from Child c " +
+			" join c.parent parent " +
+			"where parent.id = :id")
+	List<Child> findByParentIdWithExplicitJoin(@Param("id") Long id);
+
+	@Query("select c from Child c " +
+			"where c.parent.id = :id")
+	List<Child> findByParentIdWithoutExplicitJoin(@Param("id") Long id);
 }
