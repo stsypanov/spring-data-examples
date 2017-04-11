155f39de3b0f77d7c3d44676d966cb6888b24345
stsypanov <sergei.tsypanov@yandex.ru>
2017-04-11 23:04:48 +0300

    add setup

--- src/test/java/com/luxoft/logeek/DeleteAllPupilsTest.java
+++ src/test/java/com/luxoft/logeek/DeleteAllPupilsTest.java
@@ -1,12 +1,19 @@
 package com.luxoft.logeek;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
 @SuppressWarnings("unused")
 public class DeleteAllPupilsTest extends TestBase {
-    
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
     /**
      * What queries will be executed?
      */
