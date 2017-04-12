fc516e2ae1b3fbfe723ebfc283300a3cd9df130d
stsypanov <STsypanov@luxoft.com>
2017-04-12 17:40:24 +0300

    add ProjectionVsDataBenchmark

---
+++ src/main/java/com/luxoft/logeek/benchmark/ProjectionVsDataBenchmark.java
@@ -0,0 +1,41 @@
+package com.luxoft.logeek.benchmark;
+
+import com.luxoft.logeek.data.HasIdAndName;
+import com.luxoft.logeek.entity.EntityWithManyFields;
+import com.luxoft.logeek.repository.EntityWithManyFieldsRepository;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+@BenchmarkMode(value = {Mode.Throughput, Mode.AverageTime})
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+public class ProjectionVsDataBenchmark extends BenchmarkBase {
+	private EntityWithManyFieldsRepository repository;
+
+	@Setup()
+	public void init() {
+		super.initContext();
+		repository = context.getBean(EntityWithManyFieldsRepository.class);
+		List<EntityWithManyFields> entities = random
+				.longs(5000, 0, 400)
+				.boxed()
+				.map(randomLong -> new EntityWithManyFields(randomLong, String.valueOf(randomLong), "ivan"))
+				.collect(Collectors.toList());
+
+		repository.save(entities);
+	}
+
+	@Benchmark
+	public Collection<HasIdAndName> findAllByNameUsingObject() {
+		return repository.findAllByNameUsingObject("ivan");
+	}
+
+	@Benchmark
+	public Collection<HasIdAndName> findAllByName() {
+		return repository.findAllByName("ivan");
+	}
+}
--- src/main/java/com/luxoft/logeek/benchmark/runner/DistinctVsSetBenchmarkRunner.java
+++ src/main/java/com/luxoft/logeek/benchmark/runner/DistinctVsSetBenchmarkRunner.java
@@ -1,6 +1,6 @@
 package com.luxoft.logeek.benchmark.runner;
 
-import com.luxoft.logeek.benchmark.DistinctVsSetBenchmark;
+import com.luxoft.logeek.benchmark.ProjectionVsDataBenchmark;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -10,7 +10,8 @@ public class DistinctVsSetBenchmarkRunner {
 	
 	public static void main(String[] args) throws RunnerException {
 		Options opt = new OptionsBuilder()
-				.include(DistinctVsSetBenchmark.class.getSimpleName())
+//				.include(DistinctVsSetBenchmark.class.getSimpleName())
+				.include(ProjectionVsDataBenchmark.class.getSimpleName())
 				.warmupIterations(5)
 				.measurementIterations(10)
 				.forks(1)//0 makes debugging possible
