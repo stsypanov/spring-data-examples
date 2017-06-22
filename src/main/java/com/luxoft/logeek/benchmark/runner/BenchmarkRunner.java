6755e89991b15949da33f16f1ec9dcdc75fd0042
stsypanov <sergei.tsypanov@yandex.ru>
2017-06-22 23:55:15 +0300

    remove commented code

--- src/main/java/com/luxoft/logeek/benchmark/runner/BenchmarkRunner.java
+++ src/main/java/com/luxoft/logeek/benchmark/runner/BenchmarkRunner.java
@@ -1,6 +1,7 @@
 package com.luxoft.logeek.benchmark.runner;
 
 import com.luxoft.logeek.benchmark.ByteCodeInstrumentationBenchmark;
+import com.luxoft.logeek.benchmark.ByteCodeInstrumentationBenchmarkForSingleEntity;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -10,19 +11,10 @@ public class BenchmarkRunner {
 
 	public static void main(String[] args) throws RunnerException {
 		Options opt = new OptionsBuilder()
-//				.include(DistinctVsSetBenchmark.class.getSimpleName())
-//				.include(ProjectionVsDataBenchmark.class.getSimpleName())
-				.include(ByteCodeInstrumentationBenchmark.class.getSimpleName())
-//				.include(ByteCodeInstrumentationBenchmarkForSingleEntity.class.getSimpleName())
+//				.include(ByteCodeInstrumentationBenchmark.class.getSimpleName())
+				.include(ByteCodeInstrumentationBenchmarkForSingleEntity.class.getSimpleName())
 				.warmupIterations(10)
-//				.verbosity(VerboseMode.EXTRA)
 				.measurementIterations(10)
-//				.addProfiler(GCProfiler.class)
-//				.addProfiler(HotspotMemoryProfiler.class)
-//				.addProfiler(HotspotAllocationProfiler.class)
-//				.addProfiler(MemoryProfiler.class)
-//				.addProfiler(HotspotRuntimeProfiler.class)
-				.forks(1)//0 makes debugging possible
 				.build();
 
 		new Runner(opt).run();
