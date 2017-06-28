59e7e0467b26377bb0f6074834716f0f88fd9463
stsypanov <STsypanov@luxoft.com>
2017-06-28 12:39:55 +0300

    add benchmark

---
+++ src/main/java/com/luxoft/logeek/benchmark/BiteCodeInstrumentationBenchmark.java
@@ -0,0 +1,64 @@
+package com.luxoft.logeek.benchmark;
+
+import com.luxoft.logeek.entity.EntityWithManyStringFields;
+import com.luxoft.logeek.repository.EntityWithManyStringFieldsRepository;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.stream.Collectors.toList;
+
+@BenchmarkMode(value = {Mode.Throughput, Mode.AverageTime, Mode.SingleShotTime})
+//@BenchmarkMode(value = {Mode.Throughput, Mode.AverageTime})
+//@BenchmarkMode(value = {Mode.AverageTime})
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+public class BiteCodeInstrumentationBenchmark extends BenchmarkBase {
+	private EntityWithManyStringFieldsRepository repository;
+
+	@Setup
+	public void init() {
+		super.initContext();
+		repository = context.getBean(EntityWithManyStringFieldsRepository.class);
+		List<EntityWithManyStringFields> pupils = random
+				.ints(100000, 0, 400)
+				.boxed()
+				.map(rnd -> {
+					EntityWithManyStringFields entity = new EntityWithManyStringFields();
+					entity.setField1(random.nextGaussian() +"");
+					entity.setField2(random.nextGaussian() +"");
+					entity.setField3(random.nextGaussian() +"");
+					entity.setField4(random.nextGaussian() +"");
+					entity.setField5(random.nextGaussian() +"");
+					entity.setField6(random.nextGaussian() +"");
+					entity.setField7(random.nextGaussian() +"");
+					entity.setField8(random.nextGaussian() +"");
+					return entity;
+				})
+				.collect(toList());
+
+		repository.save(pupils);
+	}
+
+	@Benchmark
+	public List<EntityWithManyStringFields> measureConventionalDirtyChecking() {
+		List<EntityWithManyStringFields> entities = repository.findAll();
+		entities = entities.stream()
+				.peek(entity -> {
+					entity.setField1(random.nextGaussian() +"");
+//					entity.setField2(random.nextGaussian() +"");
+//					entity.setField3(random.nextGaussian() +"");
+//					entity.setField4(random.nextGaussian() +"");
+//					entity.setField5(random.nextGaussian() +"");
+//					entity.setField6(random.nextGaussian() +"");
+//					entity.setField7(random.nextGaussian() +"");
+//					entity.setField8(random.nextGaussian() +"");
+				})
+				.collect(toList());
+
+		entities = repository.save(entities);
+		repository.flush();
+		return entities;
+	}
+}
