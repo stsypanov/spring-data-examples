d4d7cc3b21f338d2a8cc57ab579337831e1a34d0
stsypanov <STsypanov@luxoft.com>
2017-08-19 15:37:05 +0300

    fix Test

--- src/test/java/com/luxoft/logeek/DeleteWithHandlerTest.java
+++ src/test/java/com/luxoft/logeek/DeleteWithHandlerTest.java
@@ -2,14 +2,10 @@ package com.luxoft.logeek;
 
 import com.luxoft.logeek.entity.Pupil;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.springframework.test.annotation.Commit;
-import org.springframework.test.context.TestExecutionListener;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import static com.luxoft.logeek.entity.listener.PupilListener.MESSAGE;
 import static java.util.Arrays.asList;
@@ -17,23 +13,18 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @Commit
-@Ignore
-@TestExecutionListeners(listeners = {
-        DependencyInjectionTestExecutionListener.class,
-        TestExecutionListener.class
-})
 public class DeleteWithHandlerTest extends TestBase {
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         pupilRepository.save(asList(new Pupil(9), new Pupil(10)));
     }
 
     @Test
     public void deleteAll() {
-        exception.expect(RuntimeException.class);
+        exception.expect(Exception.class);
         exception.expectMessage(MESSAGE);
         pupilRepository.deleteAll();
     }
