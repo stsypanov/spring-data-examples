064c0a9c9c7f1834db93423ebc6827dfed14692c
stsypanov <sergei.tsypanov@yandex.ru>
2018-02-04 22:19:53 +0200

    use SB in benchmark

--- src/main/java/com/luxoft/logeek/benchmark/BenchmarkBase.java
+++ src/main/java/com/luxoft/logeek/benchmark/BenchmarkBase.java
@@ -1,16 +1,18 @@
 package com.luxoft.logeek.benchmark;
 
 import com.luxoft.logeek.AppConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Random;
 
 public abstract class BenchmarkBase {
 	protected Random random;
-	protected AnnotationConfigApplicationContext context;
+	protected ConfigurableApplicationContext context;
 
 	protected void initContext() {
-		context = new AnnotationConfigApplicationContext(AppConfig.class);
+		context = SpringApplication.run(AppConfig.class);
 		random = new Random();
 	}
 }
