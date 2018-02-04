bfa47e03efac2689077478d190ee3083e242a414
stsypanov <sergei.tsypanov@yandex.ru>
2018-02-04 21:49:09 +0200

    use @DatabaseSetup

--- src/test/java/com/luxoft/logeek/DeleteChildTest.java
+++ src/test/java/com/luxoft/logeek/DeleteChildTest.java
@@ -1,39 +1,18 @@
 package com.luxoft.logeek;
 
-import com.luxoft.logeek.entity.Child;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.luxoft.logeek.entity.Parent;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.annotation.Commit;
-import org.springframework.test.context.ActiveProfiles;
 
-import static java.util.Arrays.asList;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 @Commit
-@ActiveProfiles(profiles = "oracle", inheritProfiles = false)
+@DatabaseSetup("/DeleteChildTest.xml")
 public class DeleteChildTest extends TestBase {
 
-    private Long papaId;
-
-    @Before
-    public void setUp() throws Exception {
-        Parent papa = new Parent("папа");
-
-        Child child1 = new Child(papa);
-        Child child2 = new Child(papa);
-
-        childRepository.save(asList(child1, child2));
-
-        papaId = papa.getId();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        //do not call super.tearDown();
-    }
+    private Long papaId = 1L;
 
     @Test
     public void deleteAllInBatch_expectParentIsNotDeleted() {
