a20c6847a13db32aca1e5ecf72a8c5836d100f04
stsypanov <STsypanov@luxoft.com>
2018-02-05 12:02:52 +0200

    correct StockDailyRecordRepositoryTest

--- src/test/java/com/luxoft/logeek/repository/StockDailyRecordRepositoryTest.java
+++ src/test/java/com/luxoft/logeek/repository/StockDailyRecordRepositoryTest.java
@@ -1,28 +1,28 @@
 package com.luxoft.logeek.repository;
 
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.luxoft.logeek.TestBase;
-import org.junit.Before;
+import com.luxoft.logeek.entity.StockDailyRecord;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @ActiveProfiles(value = "oracle", inheritProfiles = false)
-public class StockDailyRecordRepositoryTest extends TestBase{
-    @Autowired private StockDailyRecordRepository stockDailyRecordRepository;
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
+@DatabaseSetup("/StockDailyRecordRepositoryTest.xml")
+public class StockDailyRecordRepositoryTest extends TestBase {
+    @Autowired
+    private StockDailyRecordRepository stockDailyRecordRepository;
 
     @Test
-    public void findRateByCurrency() throws Exception {
-        BigDecimal rub = stockDailyRecordRepository.findRateByCurrency("RUB");
+    public void findRateByCurrency() {
+        StockDailyRecord record = stockDailyRecordRepository.findOne(1L);
+        BigDecimal expected = BigDecimal.valueOf(2);
 
+        assertEquals(0, record.getAverageFixedRate().compareTo(expected));
     }
 
 }
