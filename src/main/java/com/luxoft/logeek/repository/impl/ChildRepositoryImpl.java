63edb5467e24f04fcb82f26e6de1f535f88997fc
stsypanov <sergei.tsypanov@yandex.ru>
2018-02-05 22:18:42 +0200

    tiny renaming

--- src/main/java/com/luxoft/logeek/repository/impl/ChildRepositoryImpl.java
+++ src/main/java/com/luxoft/logeek/repository/impl/ChildRepositoryImpl.java
@@ -7,25 +7,25 @@ import com.luxoft.logeek.util.VelocityTemplateParser;
 import java.util.List;
 
 public class ChildRepositoryImpl extends BaseDao implements ChildRepositoryCustom {
-	
+
 	@Override
 	public List<Child> findUsingTemplate(boolean fetchParent, boolean fetchToys, boolean orderByAge) {
 		String query = VelocityTemplateParser
-				.template(ChildRepositoryTemplate.BASE_CHILD_TEMPLATE.path)
+				.template(RepositoryTemplates.BASE_CHILD_TEMPLATE.path)
 				.param("fetchParent", fetchParent)
 				.param("fetchToys", fetchToys)
 				.param("orderByAge", orderByAge)
 				.parse();
-		
+
 		return em.createQuery(query, Child.class).getResultList();
 	}
-	
-	private enum ChildRepositoryTemplate {
+
+	private enum RepositoryTemplates {
 		BASE_CHILD_TEMPLATE("BaseChildTemplate.hql.vm");
 
 		private final String path;
 
-		ChildRepositoryTemplate(String path) {
+		RepositoryTemplates(String path) {
 			this.path = "templates/" + path;
 		}
 	}
