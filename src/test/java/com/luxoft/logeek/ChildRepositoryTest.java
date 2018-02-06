32d262f98dba982d052fe8effffc6de208e5ca3b
stsypanov <sergei.tsypanov@yandex.ru>
2018-02-06 21:37:45 +0200

    uncomment test

--- src/test/java/com/luxoft/logeek/ChildRepositoryTest.java
+++ src/test/java/com/luxoft/logeek/ChildRepositoryTest.java
@@ -1,77 +1,61 @@
 package com.luxoft.logeek;
 
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.luxoft.logeek.entity.Child;
 import com.luxoft.logeek.entity.Parent;
 import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.springframework.test.annotation.Commit;
 
+import java.util.List;
+
 import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 @Commit
-@Ignore
+@DatabaseSetup("/ChildRepositoryTest.xml")
 public class ChildRepositoryTest extends TestBase {
 
-	private Long childId;
+    @Test
+    public void testNamedQuery() {
+        List<Child> children = childRepository.findByParentName("мама");
+        assertEquals(2, children.size());
+    }
 
-	@Override
-	@Before
-	@Commit
-	public void setUp() {
-		Parent papa = new Parent("папа");
-		Parent mama = new Parent("мама");
+    @Test
+    public void testQueryWithExplicitJoin() {
+        List<Child> children = childRepository.findByParentNameWithExplicitJoin("мама");
+        assertEquals(2, children.size());
+    }
 
-		Child child1 = new Child(papa);
-		Child child2 = new Child(papa);
-		Child child3 = new Child(papa);
-		Child child4 = new Child(papa);
+    @Test
+    public void testQueryWithoutExplicitJoin() {
+        List<Child> children = childRepository.findByParentNameNoExplicitJoin("мама");
+        assertEquals(2, children.size());
+    }
 
-		Child child5 = new Child(mama);
-		Child child6 = new Child(mama);
+    @Test
+    public void findByParentId() {
+        childRepository.findByParentId(1L);
+    }
 
-		childRepository.save(asList(child1, child2, child3, child4, child5, child6));
+    @Test
+    public void findByParentIdWithExplicitJoin() {
+        childRepository.findByParentIdWithExplicitJoin(1L);
+    }
 
-		childId = child1.getId();
-	}
+    @Test
+    public void findByParentIdWithoutExplicitJoin() {
+        childRepository.findByParentIdWithoutExplicitJoin(1L);
+    }
 
-	/*@Test
-	public void testNamedQuery() throws Exception {
-		List<Child> children = childRepository.findByParentName("мама");
-		assertEquals(2, children.size());
-	}
+    @Test
+    public void testFindWithTemplate() {
+        List<Child> children = childRepository.findUsingTemplate(false, false, false);
+        assertFalse(children.isEmpty());
+    }
 
-	@Test
-	public void testQueryWithExplicitJoin() throws Exception {
-		List<Child> children = childRepository.findByParentNameWithExplicitJoin("мама");
-		assertEquals(2, children.size());
-	}
 
-	@Test
-	public void testQueryWithoutExplicitJoin() throws Exception {
-		List<Child> children = childRepository.findByParentNameNoExplicitJoin("мама");
-		assertEquals(2, children.size());
-	}
-
-	@Test
-	public void findByParentId() {
-		childRepository.findByParentId(1L);
-	}
-
-	@Test
-	public void findByParentIdWithExplicitJoin() {
-		childRepository.findByParentIdWithExplicitJoin(1L);
-	}
-
-	@Test
-	public void findByParentIdWithoutExplicitJoin() {
-		childRepository.findByParentIdWithoutExplicitJoin(1L);
-	}
-
-	@Test
-	public void testFindWithTemplate() throws Exception {
-		List<Child> children = childRepository.findUsingTemplate(false, false, false);
-		assertFalse(children.isEmpty());
-	}*/
-
-	
 }
