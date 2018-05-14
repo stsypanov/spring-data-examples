560e8b6447d2ef2a824168deab0fba7b3e6d63da
stsypanov <sergei.tsypanov@yandex.ru>
2018-05-14 22:38:42 +0300

    merge examples for composite key into a single test

--- src/test/java/com/luxoft/logeek/EntityWithCompositeKeyRepositoryTest.java
+++ src/test/java/com/luxoft/logeek/EntityWithCompositeKeyRepositoryTest.java
@@ -2,8 +2,8 @@ package com.luxoft.logeek;
 
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.luxoft.logeek.entity.CompositeKey;
-import com.luxoft.logeek.entity.EntityWithCompositeKey;
 import com.luxoft.logeek.repository.EntityWithCompositeKeyRepository;
+import com.luxoft.logeek.repository.another.AnotherEntityWithCompositeKeyRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,25 +14,31 @@ import java.util.List;
 @DatabaseSetup("/EntityWithCompositeKeyRepositoryTest.xml")
 public class EntityWithCompositeKeyRepositoryTest extends TestBase {
 
-	@Autowired
-	private EntityWithCompositeKeyRepository entityWithCompositeKeyRepository;
+    @Autowired
+    private EntityWithCompositeKeyRepository entityWithCompositeKeyRepository;
+    @Autowired
+    private AnotherEntityWithCompositeKeyRepository anotherEntityWithCompositeKeyRepository;
 
-	private List<CompositeKey> compositeKeys;
+    private List<CompositeKey> compositeKeys;
 
-	@Override
-	@Before
-	public void setUp() {
-		CompositeKey key1 = new CompositeKey(1L, 1L);
-		CompositeKey key2 = new CompositeKey(2L, 2L);
-		CompositeKey key3 = new CompositeKey(3L, 3L);
-		CompositeKey key4 = new CompositeKey(4L, 4L);
-		CompositeKey key5 = new CompositeKey(5L, 5L);
+    @Before
+    public void setUp() {
+        compositeKeys = Arrays.asList(
+                new CompositeKey(1L, 1L),
+                new CompositeKey(2L, 2L),
+                new CompositeKey(3L, 3L),
+                new CompositeKey(4L, 4L),
+                new CompositeKey(5L, 5L)
+        );
+    }
 
-		compositeKeys = Arrays.asList(key1, key2, key3, key4, key5);
-	}
+    @Test//case for @EmbeddedId
+    public void findAll() {
+        entityWithCompositeKeyRepository.findAllById(compositeKeys);
+    }
 
-	@Test
-	public void findAll() {
-		List<EntityWithCompositeKey> all = entityWithCompositeKeyRepository.findAllById(compositeKeys);
-	}
+    @Test//case for @Id @Id
+    public void _findAll() {
+        anotherEntityWithCompositeKeyRepository.findAllById(compositeKeys);
+    }
 }
