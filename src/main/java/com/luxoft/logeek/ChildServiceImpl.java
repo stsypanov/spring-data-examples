46850ed691840c6f85369a307c65c6920abab0c2
stsypanov <sergei.tsypanov@yandex.ru>
2018-05-26 22:51:28 +0300

    add test for blind CrudRepository::findById

--- src/main/java/com/luxoft/logeek/ChildServiceImpl.java
+++ src/main/java/com/luxoft/logeek/ChildServiceImpl.java
@@ -2,22 +2,34 @@ package com.luxoft.logeek;
 
 import com.luxoft.logeek.entity.Child;
 import com.luxoft.logeek.repository.ChildRepository;
+import com.luxoft.logeek.repository.ParentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 /**
  * Created by Сергей on 02.04.2017.
  */
-@SuppressWarnings("unused")
 @Component
 @Transactional
 @RequiredArgsConstructor
 public class ChildServiceImpl implements ChildService {
-	private final ChildRepository childRepository;
+    private final ChildRepository childRepository;
+    private final ParentRepository parentRepository;
 
+    @Override
+    public Child newChildForParent(long parentId) {
+        Child child = new Child();
+        parentRepository.findById(parentId).ifPresent(child::setParent);
+
+        return childRepository.save(child);
+    }
+
+    @Override
+    public Child optimizedNewChildForParent(long parentId) {
+        Child child = new Child();
+        child.setParent(parentRepository.getOne(parentId));
+
+        return childRepository.save(child);
+    }
 }
--- src/test/java/com/luxoft/logeek/ChildServiceImplTest.java
+++ src/test/java/com/luxoft/logeek/ChildServiceImplTest.java
@@ -1,24 +1,38 @@
 package com.luxoft.logeek;
 
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.luxoft.logeek.entity.Child;
-import com.luxoft.logeek.entity.Parent;
-import com.luxoft.logeek.repository.ChildRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Commit;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 /**
  * Created by Сергей on 02.04.2017.
  */
+@DatabaseSetup("/ChildServiceImplTest.xml")
 public class ChildServiceImplTest extends TestBase {
     @Autowired
     private ChildService childService;
-    @Autowired
-    private ChildRepository childRepository;
 
+    @Test
+    public void newChildForParent() {
+        childService.newChildForParent(1L);
+    }
 
+    @Test
+    public void optimizedNewChildForParent() {
+        childService.optimizedNewChildForParent(1L);
+    }
+
+    @After
+    public void tearDown() {
+        List<Child> children = childRepository.findAll();
+        assertEquals(children.size(), 1);
+    }
 }
