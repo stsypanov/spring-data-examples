f9e188ffb0650ab09977269c1cae74083625ab1a
stsypanov <sergei.tsypanov@yandex.ru>
2018-08-21 21:56:22 +0300

    narrow entity scan

--- src/main/java/com/luxoft/logeek/AppConfig.java
+++ src/main/java/com/luxoft/logeek/AppConfig.java
@@ -2,11 +2,13 @@ package com.luxoft.logeek;
 
 import com.luxoft.logeek.repository.BaseJpaRepositoryImpl;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan
 @EnableAutoConfiguration
+@EntityScan(basePackages = "com.luxoft.logeek.entity")
 @EnableJpaRepositories(repositoryBaseClass = BaseJpaRepositoryImpl.class)
 public class AppConfig {
 }
