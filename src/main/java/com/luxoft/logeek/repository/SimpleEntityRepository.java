8d51778c1c3e2ee782a9cc44923633558f7c59a3
Сергей Цыпанов <sergei.tsypanov@yandex.ru>
2020-09-09 13:40:14 +0300

    Remove redundant @Param

--- src/main/java/com/luxoft/logeek/repository/SimpleEntityRepository.java
+++ src/main/java/com/luxoft/logeek/repository/SimpleEntityRepository.java
@@ -15,6 +15,6 @@ public interface SimpleEntityRepository extends BaseJpaRepository<SimpleEntity, 
   @Query("select new com.luxoft.logeek.data.IdAndNameDto(e.id, e.name)" +
     " from SimpleEntity e " +
     "where e.name = :name")
-  List<IdAndNameDto> findAllByNameUsingDto(@Param("name") String name);
+  List<IdAndNameDto> findAllByNameUsingDto(String name);
 
 }
